8ac47988133070171dcdd9725a456d924847df95
AllanNisbet <admin@stormitsolutions.com>
2017-05-04 17:43:54 +0100

    updated interface

---
+++ UOFans-Launcher/UOFans-Launcher/Controls/TabItemLink.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace UOFans_Launcher.Controls
+{
+    public class TabItemLink : TabItem
+    {
+        public string Link { get; set; }
+
+        protected override void OnMouseLeftButtonDown(MouseButtonEventArgs e)
+        {
+            e.Handled = true;
+            Process.Start(Link);
+        }
+    }
+}
